0773058a708ef9d45d528bbe4131875a99d4d65a
Sanjeev <sanjeev.kukanur@gmail.com>
2025-09-13 11:23:58 +0530

    update-code

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -63,14 +63,14 @@ const Homepage = () => {
         </div>
       </div>
 
-      <div className="container mx-auto px-4 my-2 grid grid-cols-5 md:grid-cols-8 lg:grid-cols-10 gap-2">
+      <div className="container mx-auto px-4 my-2 grid grid-cols-4 md:grid-cols-8 lg:grid-cols-10 gap-2">
         {loadingCategory
           ? new Array(12).fill(null).map((_, index) => (
               <div
                 key={index}
                 className="bg-white rounded p-4 min-h-36 grid gap-2 shadow animate-pulse"
               >
-                <div className="bg-blue-100 min-h-24 rounded" />
+                <div className="bg-blue-100 min-h-30 rounded" />
                 <div className="bg-blue-100 h-8 rounded" />
               </div>
             ))
@@ -91,7 +91,7 @@ const Homepage = () => {
             ))}
       </div>
 
-      <div className="container mx-auto px-4 my-4 flex gap-4"></div>
+      {/* <div className="container mx-auto px-4 my-4 flex gap-4"></div> */}
 {/***display category product */}
       {/* {
         categoryData?.data?.map((category,index)=>{
